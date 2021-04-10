6ef9a8f570a6322566c4c29c2601d0c35826d034
shalitha <shalithaanuradha123@gmail.com>
2021-04-10 20:32:00 +0530

    Add styles from scss file and scripts from main.ts file as imports.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,23 @@
 // import style from './style.scss';
 import './app/app.module.js';
 
+import '../node_modules/admin-lte/dist/js/pages/dashboard.js';
+import '../node_modules/admin-lte/plugins/jquery/jquery.min.js';
+import '../node_modules/admin-lte/plugins/jquery-ui/jquery-ui.min.js';
+import '../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import '../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';
+import '../node_modules/admin-lte/dist/js/adminlte.js';
+import '../node_modules/admin-lte/dist/js/demo.js';
+
+
+import style from './style.scss';
+import '../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import '../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';
+import '../node_modules/admin-lte/dist/js/adminlte.js';
+
+var html = '<style>' + style + '</style>';
+$("head").append(html);
+
 
 var abc=import('jquery')
 abc.then(function (RS) {
